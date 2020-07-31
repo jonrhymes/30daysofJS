2b52ee60b03de502e320a65569141a1c3b47f000
Jon Rhymes <jonrhymes@Alisons-MacBook-Air.local>
2020-07-31 12:06:36 -0400

    Video player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,39 @@
+/* Get our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build our functions */
+function togglePlay() {
+    // const method = video.paused ? 'play' : 'pause';
+    // video[method]();
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon)
+    toggle.textContent = icon;
+    console.log('Update the button')
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+/* Hook up the even listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
